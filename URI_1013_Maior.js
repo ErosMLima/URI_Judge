a43db09f04c4e5ea1e25bcc45abf39b5c2d59e06
Eros M.Lima <60955355+ErosMLima@users.noreply.github.com>
2022-04-11 15:14:43 -0300

    Finding the larger number
    Finding the larger number

---
+++ URI_1013_Maior.js
@@ -0,0 +1,6 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+const [A, B, C] = input.split(" ").map(item => parseInt(item));
+
+const maiorAB = (A + B + Math.abs(A - B)) / 2;
+const maiorXC = ( maiorXC + "eh o maior");
